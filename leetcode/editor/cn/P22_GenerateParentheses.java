533fa49eb1edd3d7f47340f4d981d37bf119b7cd
丁生 <fabian4@163.com>
2021-07-28 17:22:41 +0800

    update

---
+++ leetcode/editor/cn/P22_GenerateParentheses.java
@@ -0,0 +1,77 @@
+//数字 n 代表生成括号的对数，请你设计一个函数，用于能够生成所有可能的并且 有效的 括号组合。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：n = 3
+//输出：["((()))","(()())","(())()","()(())","()()()"]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：n = 1
+//输出：["()"]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= n <= 8 
+// 
+// Related Topics 字符串 动态规划 回溯 
+// 👍 1915 👎 0
+
+
+package leetcode.editor.cn;
+
+//括号生成
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author fabian
+ * @date 2021-07-28 17:17:09
+ */
+public class P22_GenerateParentheses {
+    public static void main(String[] args) {
+        Solution solution = new P22_GenerateParentheses().new Solution();
+
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        List<String> res = new ArrayList<>();
+        public List<String> generateParenthesis(int n) {
+            if(n <= 0){
+                return res;
+            }
+            getParenthesis("",n,n);
+            return res;
+        }
+
+        private void getParenthesis(String str,int left, int right) {
+            if(left == 0 && right == 0 ){
+                res.add(str);
+                return;
+            }
+            if(left == right){
+                //剩余左右括号数相等，下一个只能用左括号
+                getParenthesis(str+"(",left-1,right);
+            }else if(left < right){
+                //剩余左括号小于右括号，下一个可以用左括号也可以用右括号
+                if(left > 0){
+                    getParenthesis(str+"(",left-1,right);
+                }
+                getParenthesis(str+")",left,right-1);
+            }
+        }
+    }
+    //leetcode submit region end(Prohibit modification and deletion)
+
+}

3cfc7d363f2f58439e8e48f41dce05c488e6b825
丁生 <fabian4@163.com>
2021-07-29 18:05:53 +0800

    update

--- leetcode/editor/cn/P41_FirstMissingPositive.java
+++ leetcode/editor/cn/P41_FirstMissingPositive.java
@@ -44,17 +44,35 @@ package leetcode.editor.cn;
  * @author fabian
  * @date 2021-07-29 17:50:09
  */
-public class P41_FirstMissingPositive{
-	 public static void main(String[] args) {
-	 	 Solution solution = new P41_FirstMissingPositive().new Solution();
-	 
-	 }
-	//leetcode submit region begin(Prohibit modification and deletion)
-	class Solution {
-		public int firstMissingPositive(int[] nums) {
+public class P41_FirstMissingPositive {
+    public static void main(String[] args) {
+        Solution solution = new P41_FirstMissingPositive().new Solution();
 
-		}
-	}
-	//leetcode submit region end(Prohibit modification and deletion)
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int firstMissingPositive(int[] nums) {
+            int n = nums.length;
+            for (int i = 0; i < n; ++i) {
+                if (nums[i] <= 0) {
+                    nums[i] = n + 1;
+                }
+            }
+            for (int i = 0; i < n; ++i) {
+                int num = Math.abs(nums[i]);
+                if (num <= n) {
+                    nums[num - 1] = -Math.abs(nums[num - 1]);
+                }
+            }
+            for (int i = 0; i < n; ++i) {
+                if (nums[i] > 0) {
+                    return i + 1;
+                }
+            }
+            return n + 1;
+        }
+    }
+    //leetcode submit region end(Prohibit modification and deletion)
 
 }

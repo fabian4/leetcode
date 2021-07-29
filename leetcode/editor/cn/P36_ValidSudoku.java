7afb0b7ce65790cff32f5980525836f494473add
丁生 <fabian4@163.com>
2021-07-29 17:24:25 +0800

    update

--- leetcode/editor/cn/P36_ValidSudoku.java
+++ leetcode/editor/cn/P36_ValidSudoku.java
@@ -81,26 +81,27 @@ public class P36_ValidSudoku {
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public boolean isValidSudoku(char[][] board) {
-            int [][]row  =new int[9][10];
-            int [][]col  =new int[9][10];
-            int [][]box  =new int[9][10];
+            int[][] row = new int[9][10];
+            int[][] col = new int[9][10];
+            int[][] box = new int[9][10];
             for (int i = 0; i < 9; i++) {
                 for (int j = 0; j < 9; j++) {
-                    if (board[i][j]=='.'){
+                    if (board[i][j] == '.') {
                         continue;
                     }
-                    int curNum = board[i][j]-'0';
-                    if (row[i][curNum]==1){
-                        return false;
-                    }if (col[j][curNum]==1){
+                    int curNum = board[i][j] - '0';
+                    if (row[i][curNum] == 1) {
                         return false;
                     }
-                    if (box[j/3 + (i/3) * 3][curNum]==1){
+                    if (col[j][curNum] == 1) {
                         return false;
                     }
-                    row[i][curNum]=1;
-                    col[j][curNum]=1;
-                    box[j/3 + (i/3) * 3][curNum]=1;
+                    if (box[j / 3 + (i / 3) * 3][curNum] == 1) {
+                        return false;
+                    }
+                    row[i][curNum] = 1;
+                    col[j][curNum] = 1;
+                    box[j / 3 + (i / 3) * 3][curNum] = 1;
                 }
             }
             return true;

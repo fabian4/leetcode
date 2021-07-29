5657eda2e85642b38e2eea8a67e63ef32108cf46
丁生 <fabian4@163.com>
2021-07-29 15:10:52 +0800

    update

--- leetcode/editor/cn/P29_DivideTwoIntegers.java
+++ leetcode/editor/cn/P29_DivideTwoIntegers.java
@@ -42,13 +42,55 @@ package leetcode.editor.cn;
 public class P29_DivideTwoIntegers {
     public static void main(String[] args) {
         Solution solution = new P29_DivideTwoIntegers().new Solution();
-
+        System.out.println(Math.abs(-2147483648));
+        solution.divide(-2147483648, -1);
     }
 
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public int divide(int dividend, int divisor) {
+            if (dividend == 0) {
+                return 0;
+            }
+            if (dividend == divisor) {
+                return 1;
+            }
+            if (dividend == Integer.MIN_VALUE && divisor == -1) {
+                return Integer.MAX_VALUE;
+            }
+            boolean diffSign = false;
+            if ((dividend < 0) != (divisor < 0)) {
+                diffSign = true;
+            }
+            int i = 0;
+            int j = 0;
+            int sum = 0;
+            int minus = 0;
+            for (; ; ) {
+                int temp = 0;
+                minus = divisor << j;
+                if (diffSign) {
+                    temp = sum - minus;
+                } else {
+                    temp = sum + minus;
+                }
+                //需要考虑temp溢出的情况，因此判断条件加一个temp是否变号
+                if ((dividend > 0 && (temp > dividend || temp < sum)) || (dividend < 0 && (temp < dividend || temp > sum))) {
+                    if (j == 0) {
+                        break; //即使只加一个除数也会超过被除数，因此退出循环。
+                    }
+                    j--; //试探增加量减半的情况
+                    continue;
+                }
+                sum = temp;
+                i += (1 << j); //累加被除数的个数
+                j++;
+            }
 
+            if (diffSign) {
+                return -i;
+            }
+            return i;
         }
     }
     //leetcode submit region end(Prohibit modification and deletion)

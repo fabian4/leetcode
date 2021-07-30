6bd3f0f9ec8bd3075562ceda17f86605028c9575
丁生 <fabian4@163.com>
2021-07-30 15:30:46 +0800

    update

--- leetcode/editor/cn/P49_GroupAnagrams.java
+++ leetcode/editor/cn/P49_GroupAnagrams.java
@@ -40,7 +40,7 @@ package leetcode.editor.cn;
 
 //字母异位词分组
 
-import java.util.List;
+import java.util.*;
 
 /**
  * @author fabian
@@ -55,7 +55,27 @@ public class P49_GroupAnagrams {
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public List<List<String>> groupAnagrams(String[] strs) {
-
+            Map<String, List<String>> map = new HashMap<String, List<String>>();
+            for (String str : strs) {
+                int[] counts = new int[26];
+                int length = str.length();
+                for (int i = 0; i < length; i++) {
+                    counts[str.charAt(i) - 'a']++;
+                }
+                // 将每个出现次数大于 0 的字母和出现次数按顺序拼接成字符串，作为哈希表的键
+                StringBuffer sb = new StringBuffer();
+                for (int i = 0; i < 26; i++) {
+                    if (counts[i] != 0) {
+                        sb.append((char) ('a' + i));
+                        sb.append(counts[i]);
+                    }
+                }
+                String key = sb.toString();
+                List<String> list = map.getOrDefault(key, new ArrayList<String>());
+                list.add(str);
+                map.put(key, list);
+            }
+            return new ArrayList<List<String>>(map.values());
         }
     }
     //leetcode submit region end(Prohibit modification and deletion)

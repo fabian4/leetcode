cfa340b5c02462311d7e75baa1a2ef16cb1f2839
丁生 <fabian4@163.com>
2021-07-30 18:49:56 +0800

    update

--- leetcode/editor/cn/P54_SpiralMatrix.java
+++ leetcode/editor/cn/P54_SpiralMatrix.java
@@ -52,37 +52,37 @@ public class P54_SpiralMatrix {
         public List<Integer> spiralOrder(int[][] matrix) {
             List<Integer> ans = new LinkedList<>();
             int top = 0;
-            int bottom = matrix.length-1;
+            int bottom = matrix.length - 1;
             int left = 0;
-            int right = matrix[0].length-1;
+            int right = matrix[0].length - 1;
             int size = matrix.length * matrix[0].length;
             while (ans.size() < size) {
 
                 for (int i = left; i <= right; i++) {
                     ans.add(matrix[top][i]);
                 }
-                if(++top > bottom) {
+                if (++top > bottom) {
                     break;
                 }
 
                 for (int i = top; i <= bottom; i++) {
                     ans.add(matrix[i][right]);
                 }
-                if(--right < left) {
+                if (--right < left) {
                     break;
                 }
 
                 for (int i = right; i >= left; i--) {
                     ans.add(matrix[bottom][i]);
                 }
-                if(--bottom < top) {
+                if (--bottom < top) {
                     break;
                 }
 
                 for (int i = bottom; i >= top; i--) {
                     ans.add(matrix[i][left]);
                 }
-                if(++left > right) {
+                if (++left > right) {
                     break;
                 }
             }

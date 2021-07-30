907d122586b43f9da73b20a27b7a8ed21d8ba689
丁生 <fabian4@163.com>
2021-07-30 15:15:58 +0800

    update

--- leetcode/editor/cn/P48_RotateImage.java
+++ leetcode/editor/cn/P48_RotateImage.java
@@ -63,16 +63,16 @@ public class P48_RotateImage {
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public void rotate(int[][] matrix) {
-			int n = matrix.length;
-			for (int i = 0; i < n / 2; ++i) {
-				for (int j = 0; j < (n + 1) / 2; ++j) {
-					int temp = matrix[i][j];
-					matrix[i][j] = matrix[n - j - 1][i];
-					matrix[n - j - 1][i] = matrix[n - i - 1][n - j - 1];
-					matrix[n - i - 1][n - j - 1] = matrix[j][n - i - 1];
-					matrix[j][n - i - 1] = temp;
-				}
-			}
+            int n = matrix.length;
+            for (int i = 0; i < n / 2; ++i) {
+                for (int j = 0; j < (n + 1) / 2; ++j) {
+                    int temp = matrix[i][j];
+                    matrix[i][j] = matrix[n - j - 1][i];
+                    matrix[n - j - 1][i] = matrix[n - i - 1][n - j - 1];
+                    matrix[n - i - 1][n - j - 1] = matrix[j][n - i - 1];
+                    matrix[j][n - i - 1] = temp;
+                }
+            }
         }
     }
     //leetcode submit region end(Prohibit modification and deletion)
---
+++ leetcode/editor/cn/P49_GroupAnagrams.java
@@ -0,0 +1,63 @@
+//给定一个字符串数组，将字母异位词组合在一起。可以按任意顺序返回结果列表。 
+//
+// 字母异位词指字母相同，但排列不同的字符串。 
+//
+// 
+//
+// 示例 1: 
+//
+// 
+//输入: strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
+//输出: [["bat"],["nat","tan"],["ate","eat","tea"]] 
+//
+// 示例 2: 
+//
+// 
+//输入: strs = [""]
+//输出: [[""]]
+// 
+//
+// 示例 3: 
+//
+// 
+//输入: strs = ["a"]
+//输出: [["a"]] 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= strs.length <= 104 
+// 0 <= strs[i].length <= 100 
+// strs[i] 仅包含小写字母 
+// 
+// Related Topics 哈希表 字符串 排序 
+// 👍 802 👎 0
+
+
+package leetcode.editor.cn;
+
+//字母异位词分组
+
+import java.util.List;
+
+/**
+ * @author fabian
+ * @date 2021-07-30 15:15:35
+ */
+public class P49_GroupAnagrams {
+    public static void main(String[] args) {
+        Solution solution = new P49_GroupAnagrams().new Solution();
+
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public List<List<String>> groupAnagrams(String[] strs) {
+
+        }
+    }
+    //leetcode submit region end(Prohibit modification and deletion)
+
+}

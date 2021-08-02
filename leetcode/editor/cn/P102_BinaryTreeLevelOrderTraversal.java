a616ea1bef6d583846033263e8ec9bbd449812af
丁生 <fabian4@163.com>
2021-08-02 16:49:01 +0800

    update

--- leetcode/editor/cn/P102_BinaryTreeLevelOrderTraversal.java
+++ leetcode/editor/cn/P102_BinaryTreeLevelOrderTraversal.java
@@ -1,28 +1,28 @@
-//给你一个二叉树，请你返回其按 层序遍历 得到的节点值。 （即逐层地，从左到右访问所有节点）。 
+//给你一个二叉树，请你返回其按 层序遍历 得到的节点值。 （即逐层地，从左到右访问所有节点）。
 //
-// 
 //
-// 示例： 
-//二叉树：[3,9,20,null,null,15,7], 
 //
-// 
+// 示例：
+//二叉树：[3,9,20,null,null,15,7],
+//
+//
 //    3
 //   / \
 //  9  20
 //    /  \
 //   15   7
-// 
 //
-// 返回其层序遍历结果： 
 //
-// 
+// 返回其层序遍历结果：
+//
+//
 //[
 //  [3],
 //  [9,20],
 //  [15,7]
 //]
-// 
-// Related Topics 树 广度优先搜索 二叉树 
+//
+// Related Topics 树 广度优先搜索 二叉树
 // 👍 952 👎 0
 
 
@@ -32,6 +32,8 @@ package leetcode.editor.cn;
 
 import leetcode.editor.cn.base.TreeNode;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 
@@ -39,33 +41,54 @@ import java.util.Queue;
  * @author fabian
  * @date 2021-08-02 11:26:20
  */
-public class P102_BinaryTreeLevelOrderTraversal{
-	 public static void main(String[] args) {
-	 	 Solution solution = new P102_BinaryTreeLevelOrderTraversal().new Solution();
-	 
-	 }
-	//leetcode submit region begin(Prohibit modification and deletion)
-	/**
-	 * Definition for a binary tree node.
-	 * public class TreeNode {
-	 *     int val;
-	 *     TreeNode left;
-	 *     TreeNode right;
-	 *     TreeNode() {}
-	 *     TreeNode(int val) { this.val = val; }
-	 *     TreeNode(int val, TreeNode left, TreeNode right) {
-	 *         this.val = val;
-	 *         this.left = left;
-	 *         this.right = right;
-	 *     }
-	 * }
-	 */
-	class Solution {
-		public List<List<Integer>> levelOrder(TreeNode root) {
-			Queue<TreeNode> queue
+public class P102_BinaryTreeLevelOrderTraversal {
+    public static void main(String[] args) {
+        Solution solution = new P102_BinaryTreeLevelOrderTraversal().new Solution();
 
-		}
-	}
-	//leetcode submit region end(Prohibit modification and deletion)
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     * int val;
+     * TreeNode left;
+     * TreeNode right;
+     * TreeNode() {}
+     * TreeNode(int val) { this.val = val; }
+     * TreeNode(int val, TreeNode left, TreeNode right) {
+     * this.val = val;
+     * this.left = left;
+     * this.right = right;
+     * }
+     * }
+     */
+    class Solution {
+        public List<List<Integer>> levelOrder(TreeNode root) {
+            List<List<Integer>> list = new ArrayList<>();
+            if (root == null) {
+                return list;
+            }
+            Queue<TreeNode> queue = new LinkedList<>();
+            queue.offer(root);
+            while (!queue.isEmpty()) {
+                List<Integer> level = new ArrayList<>();
+                int size = queue.size();
+                for (int i = 0; i < size; i++) {
+                    TreeNode p = queue.poll();
+                    level.add(p.val);
+                    if(p.left!=null){
+                        queue.offer(p.left);
+                    }
+                    if(p.right!=null){
+                        queue.offer(p.right);
+                    }
+                }
+                list.add(level);
+            }
+            return list;
+        }
+    }
+    //leetcode submit region end(Prohibit modification and deletion)
 
 }

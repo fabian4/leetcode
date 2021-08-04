0fcdba9c8e6467d39e828a5a3c58917d4f45dab7
丁生 <fabian4@163.com>
2021-08-04 22:52:27 +0800

    update

---
+++ leetcode/editor/cn/base/Node.java
@@ -0,0 +1,26 @@
+package leetcode.editor.cn.base;
+
+/**
+ * @author fabian
+ * @date 2021-08-04 22:39
+ */
+public class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {
+    }
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+}

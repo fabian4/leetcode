89aa3b83468c2da0115d2defcfb190e7687bd289
丁生 <fabian4@163.com>
2021-08-11 22:18:15 +0800

    update

---
+++ leetcode/editor/cn/P149_MaxPointsOnALine.java
@@ -0,0 +1,101 @@
+//给你一个数组 points ，其中 points[i] = [xi, yi] 表示 X-Y 平面上的一个点。求最多有多少个点在同一条直线上。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：points = [[1,1],[2,2],[3,3]]
+//输出：3
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：points = [[1,1],[3,2],[5,3],[4,1],[2,3],[1,4]]
+//输出：4
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= points.length <= 300 
+// points[i].length == 2 
+// -104 <= xi, yi <= 104 
+// points 中的所有点 互不相同 
+// 
+// Related Topics 几何 哈希表 数学 
+// 👍 337 👎 0
+
+
+package leetcode.editor.cn;
+
+//直线上最多的点数
+
+import leetcode.editor.cn.base.ListNode;
+
+/**
+ * @author fabian
+ * @date 2021-08-11 22:11:28
+ */
+public class P149_MaxPointsOnALine {
+    public static void main(String[] args) {
+        Solution solution = new P149_MaxPointsOnALine().new Solution();
+
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public ListNode sortList(ListNode head) {
+            return sortList(head, null);
+        }
+
+        public ListNode sortList(ListNode head, ListNode tail) {
+            if (head == null) {
+                return head;
+            }
+            if (head.next == tail) {
+                head.next = null;
+                return head;
+            }
+            ListNode slow = head, fast = head;
+            while (fast != tail) {
+                slow = slow.next;
+                fast = fast.next;
+                if (fast != tail) {
+                    fast = fast.next;
+                }
+            }
+            ListNode mid = slow;
+            ListNode list1 = sortList(head, mid);
+            ListNode list2 = sortList(mid, tail);
+            ListNode sorted = merge(list1, list2);
+            return sorted;
+        }
+
+        public ListNode merge(ListNode head1, ListNode head2) {
+            ListNode dummyHead = new ListNode(0);
+            ListNode temp = dummyHead, temp1 = head1, temp2 = head2;
+            while (temp1 != null && temp2 != null) {
+                if (temp1.val <= temp2.val) {
+                    temp.next = temp1;
+                    temp1 = temp1.next;
+                } else {
+                    temp.next = temp2;
+                    temp2 = temp2.next;
+                }
+                temp = temp.next;
+            }
+            if (temp1 != null) {
+                temp.next = temp1;
+            } else if (temp2 != null) {
+                temp.next = temp2;
+            }
+            return dummyHead.next;
+        }
+    }
+    //leetcode submit region end(Prohibit modification and deletion)
+
+}

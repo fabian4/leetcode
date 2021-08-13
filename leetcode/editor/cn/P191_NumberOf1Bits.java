a92c99e0d0fa9dc52c90499e0e2cc448cdebb21b
丁生 <fabian4@163.com>
2021-08-13 20:53:31 +0800

    update

--- leetcode/editor/cn/P191_NumberOf1Bits.java
+++ leetcode/editor/cn/P191_NumberOf1Bits.java
@@ -74,7 +74,13 @@ public class P191_NumberOf1Bits{
 	public class Solution {
 		// you need to treat n as an unsigned value
 		public int hammingWeight(int n) {
-
+			int ret = 0;
+			for (int i = 0; i < 32; i++) {
+				if ((n & (1 << i)) != 0) {
+					ret++;
+				}
+			}
+			return ret;
 		}
 	}
 	//leetcode submit region end(Prohibit modification and deletion)

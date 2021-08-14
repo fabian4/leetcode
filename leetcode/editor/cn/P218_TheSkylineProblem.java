7d3a35d346515cdf366d93149b4ff5622efa5bde
丁生 <fabian4@163.com>
2021-08-14 23:06:34 +0800

    update

--- leetcode/editor/cn/P218_TheSkylineProblem.java
+++ leetcode/editor/cn/P218_TheSkylineProblem.java
@@ -51,21 +51,48 @@ package leetcode.editor.cn;
 
 //天际线问题
 
+import java.util.*;
+
 /**
  * @author fabian
  * @date 2021-08-14 23:02:05
  */
-public class P218_TheSkylineProblem{
-	 public static void main(String[] args) {
-	 	 Solution solution = new P218_TheSkylineProblem().new Solution();
-	 
-	 }
-	//leetcode submit region begin(Prohibit modification and deletion)
-class Solution {
-    public List<List<Integer>> getSkyline(int[][] buildings) {
+public class P218_TheSkylineProblem {
+    public static void main(String[] args) {
+        Solution solution = new P218_TheSkylineProblem().new Solution();
 
     }
-}
-//leetcode submit region end(Prohibit modification and deletion)
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public List<List<Integer>> getSkyline(int[][] buildings) {
+            PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> b[1] - a[1]);
+            List<Integer> boundaries = new ArrayList<>();
+            for (int[] building : buildings) {
+                boundaries.add(building[0]);
+                boundaries.add(building[1]);
+            }
+            Collections.sort(boundaries);
+
+            List<List<Integer>> ret = new ArrayList<>();
+            int n = buildings.length, idx = 0;
+            for (int boundary : boundaries) {
+                while (idx < n && buildings[idx][0] <= boundary) {
+                    pq.offer(new int[]{buildings[idx][1], buildings[idx][2]});
+                    idx++;
+                }
+                while (!pq.isEmpty() && pq.peek()[0] <= boundary) {
+                    pq.poll();
+                }
+
+                int maxn = pq.isEmpty() ? 0 : pq.peek()[1];
+                if (ret.size() == 0 || maxn != ret.get(ret.size() - 1).get(1)) {
+                    ret.add(Arrays.asList(boundary, maxn));
+                }
+            }
+            return ret;
+        }
+    }
+    //leetcode submit region end(Prohibit modification and deletion)
 
 }

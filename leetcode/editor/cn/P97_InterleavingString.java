d16fc3e6a2cb26eda417c743e8d86866e5077a7f
丁生 <fabian4@163.com>
2021-08-21 10:21:19 +0800

    update

--- leetcode/editor/cn/P97_InterleavingString.java
+++ leetcode/editor/cn/P97_InterleavingString.java
@@ -55,17 +55,39 @@ package leetcode.editor.cn;
  * @author fabian
  * @date 2021-08-21 10:18:55
  */
-public class P97_InterleavingString{
-	 public static void main(String[] args) {
-	 	 Solution solution = new P97_InterleavingString().new Solution();
-	 
-	 }
-	//leetcode submit region begin(Prohibit modification and deletion)
-class Solution {
-    public boolean isInterleave(String s1, String s2, String s3) {
+public class P97_InterleavingString {
+    public static void main(String[] args) {
+        Solution solution = new P97_InterleavingString().new Solution();
 
     }
-}
-//leetcode submit region end(Prohibit modification and deletion)
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public boolean isInterleave(String s1, String s2, String s3) {
+            int n = s1.length(), m = s2.length(), t = s3.length();
+
+            if (n + m != t) {
+                return false;
+            }
+
+            boolean[][] f = new boolean[n + 1][m + 1];
+
+            f[0][0] = true;
+            for (int i = 0; i <= n; ++i) {
+                for (int j = 0; j <= m; ++j) {
+                    int p = i + j - 1;
+                    if (i > 0) {
+                        f[i][j] = f[i][j] || (f[i - 1][j] && s1.charAt(i - 1) == s3.charAt(p));
+                    }
+                    if (j > 0) {
+                        f[i][j] = f[i][j] || (f[i][j - 1] && s2.charAt(j - 1) == s3.charAt(p));
+                    }
+                }
+            }
+
+            return f[n][m];
+        }
+    }
+    //leetcode submit region end(Prohibit modification and deletion)
 
 }

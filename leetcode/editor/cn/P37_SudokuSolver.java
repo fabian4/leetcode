d0c0e16a0a037f36ee69f578358d24d448d4cf3c
丁生 <fabian4@163.com>
2021-08-22 19:21:43 +0800

    update

--- leetcode/editor/cn/P37_SudokuSolver.java
+++ leetcode/editor/cn/P37_SudokuSolver.java
@@ -53,6 +53,9 @@ package leetcode.editor.cn;
 
 //解数独
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author fabian
  * @date 2021-08-22 19:18:06
@@ -65,8 +68,43 @@ public class P37_SudokuSolver {
 
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
-        public void solveSudoku(char[][] board) {
+        private boolean[][] line = new boolean[9][9];
+        private boolean[][] column = new boolean[9][9];
+        private boolean[][][] block = new boolean[3][3][9];
+        private boolean valid = false;
+        private List<int[]> spaces = new ArrayList<>();
 
+        public void solveSudoku(char[][] board) {
+            for (int i = 0; i < 9; ++i) {
+                for (int j = 0; j < 9; ++j) {
+                    if (board[i][j] == '.') {
+                        spaces.add(new int[]{i, j});
+                    } else {
+                        int digit = board[i][j] - '0' - 1;
+                        line[i][digit] = column[j][digit] = block[i / 3][j / 3][digit] = true;
+                    }
+                }
+            }
+
+            dfs(board, 0);
+        }
+
+        public void dfs(char[][] board, int pos) {
+            if (pos == spaces.size()) {
+                valid = true;
+                return;
+            }
+
+            int[] space = spaces.get(pos);
+            int i = space[0], j = space[1];
+            for (int digit = 0; digit < 9 && !valid; ++digit) {
+                if (!line[i][digit] && !column[j][digit] && !block[i / 3][j / 3][digit]) {
+                    line[i][digit] = column[j][digit] = block[i / 3][j / 3][digit] = true;
+                    board[i][j] = (char) (digit + '0' + 1);
+                    dfs(board, pos + 1);
+                    line[i][digit] = column[j][digit] = block[i / 3][j / 3][digit] = false;
+                }
+            }
         }
     }
     //leetcode submit region end(Prohibit modification and deletion)

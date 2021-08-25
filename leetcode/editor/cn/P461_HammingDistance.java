b8aee5a08dac2b26847272f4d0ebc0e57655a48e
丁生 <fabian4@163.com>
2021-08-25 21:14:22 +0800

    update

--- leetcode/editor/cn/P461_HammingDistance.java
+++ leetcode/editor/cn/P461_HammingDistance.java
@@ -50,7 +50,7 @@ public class P461_HammingDistance {
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public int hammingDistance(int x, int y) {
-
+            return Integer.bitCount(x ^ y);
         }
     }
     //leetcode submit region end(Prohibit modification and deletion)

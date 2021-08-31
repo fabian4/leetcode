dd7b70035f9dc3b0daf6f6fcb104437c05adcb5d
丁生 <fabian4@163.com>
2021-08-31 21:19:36 +0800

    update

--- leetcode/editor/cn/P328_OddEvenLinkedList.java
+++ leetcode/editor/cn/P328_OddEvenLinkedList.java
@@ -26,31 +26,34 @@ package leetcode.editor.cn;
 
 //奇偶链表
 
+import leetcode.editor.cn.base.ListNode;
+
 /**
  * @author fabian
  * @date 2021-08-31 21:18:23
  */
-public class P328_OddEvenLinkedList{
-	 public static void main(String[] args) {
-	 	 Solution solution = new P328_OddEvenLinkedList().new Solution();
-	 
-	 }
-	//leetcode submit region begin(Prohibit modification and deletion)
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
-class Solution {
-    public ListNode oddEvenList(ListNode head) {
+public class P328_OddEvenLinkedList {
+    public static void main(String[] args) {
+        Solution solution = new P328_OddEvenLinkedList().new Solution();
 
     }
-}
-//leetcode submit region end(Prohibit modification and deletion)
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode() {}
+     * ListNode(int val) { this.val = val; }
+     * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
+    class Solution {
+        public ListNode oddEvenList(ListNode head) {
+
+        }
+    }
+    //leetcode submit region end(Prohibit modification and deletion)
 
 }

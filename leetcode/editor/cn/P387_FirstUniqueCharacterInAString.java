e3aff30c1062c9cde8050bd6b0fb22aa99d98297
丁生 <fabian4@163.com>
2021-09-01 21:41:27 +0800

    update

---
+++ leetcode/editor/cn/P387_FirstUniqueCharacterInAString.java
@@ -0,0 +1,42 @@
+//给定一个字符串，找到它的第一个不重复的字符，并返回它的索引。如果不存在，则返回 -1。 
+//
+// 
+//
+// 示例： 
+//
+// s = "leetcode"
+//返回 0
+//
+//s = "loveleetcode"
+//返回 2
+// 
+//
+// 
+//
+// 提示：你可以假定该字符串只包含小写字母。 
+// Related Topics 队列 哈希表 字符串 计数 👍 430 👎 0
+
+
+package leetcode.editor.cn;
+
+//字符串中的第一个唯一字符
+
+/**
+ * @author fabian
+ * @date 2021-09-01 21:41:07
+ */
+public class P387_FirstUniqueCharacterInAString {
+    public static void main(String[] args) {
+        Solution solution = new P387_FirstUniqueCharacterInAString().new Solution();
+
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int firstUniqChar(String s) {
+
+        }
+    }
+    //leetcode submit region end(Prohibit modification and deletion)
+
+}

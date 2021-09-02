db14f365ff0a06ff59f51aa222085757f70022ab
丁生 <fabian4@163.com>
2021-09-02 20:26:22 +0800

    update

---
+++ leetcode/editor/cn/P412_FizzBuzz.java
@@ -0,0 +1,56 @@
+//写一个程序，输出从 1 到 n 数字的字符串表示。 
+//
+// 1. 如果 n 是3的倍数，输出“Fizz”； 
+//
+// 2. 如果 n 是5的倍数，输出“Buzz”； 
+//
+// 3.如果 n 同时是3和5的倍数，输出 “FizzBuzz”。 
+//
+// 示例： 
+//
+// n = 15,
+//
+//返回:
+//[
+//    "1",
+//    "2",
+//    "Fizz",
+//    "4",
+//    "Buzz",
+//    "Fizz",
+//    "7",
+//    "8",
+//    "Fizz",
+//    "Buzz",
+//    "11",
+//    "Fizz",
+//    "13",
+//    "14",
+//    "FizzBuzz"
+//]
+// 
+// Related Topics 数学 字符串 模拟 👍 103 👎 0
+
+
+package leetcode.editor.cn;
+
+//Fizz Buzz
+
+/**
+ * @author fabian
+ * @date 2021-09-02 20:25:24
+ */
+public class P412_FizzBuzz{
+	 public static void main(String[] args) {
+	 	 Solution solution = new P412_FizzBuzz().new Solution();
+	 
+	 }
+	//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public List<String> fizzBuzz(int n) {
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}

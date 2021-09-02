0ea6f5e1c3720c35a8c13a087a621b2535b1661c
丁生 <fabian4@163.com>
2021-09-02 20:33:57 +0800

    update

--- leetcode/editor/cn/P412_FizzBuzz.java
+++ leetcode/editor/cn/P412_FizzBuzz.java
@@ -36,6 +36,7 @@ package leetcode.editor.cn;
 
 //Fizz Buzz
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -51,7 +52,19 @@ public class P412_FizzBuzz {
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public List<String> fizzBuzz(int n) {
-
+            List<String> list = new ArrayList<>();
+            for (int i = 1; i <= n; i++) {
+                if (i % 15 == 0) {
+                    list.add("FizzBuzz");
+                } else if (i % 5 == 0) {
+                    list.add("Buzz");
+                } else if (i % 3 == 0) {
+                    list.add("Fizz");
+                } else {
+                    list.add(String.valueOf(i));
+                }
+            }
+            return list;
         }
     }
     //leetcode submit region end(Prohibit modification and deletion)

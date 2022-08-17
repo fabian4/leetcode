2dbfad322288efb1e7d3abab4b81e45d3b918f3a
fabian4 <fabian4@163.com>
2022-08-17 15:10:16 +0800

    update

--- leetcode/editor/cn/P39_CombinationSum.java
+++ leetcode/editor/cn/P39_CombinationSum.java
@@ -44,27 +44,51 @@
 // Related Topics 数组 回溯 👍 2116 👎 0
 
 
-  package leetcode.editor.cn;
+package leetcode.editor.cn;
 
-  //组合总和
+//组合总和
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
-  * @author fabian
-  * @date 2022-08-17 11:33:00
-  */
-  public class P39_CombinationSum{
-  public static void main(String[] args) {
-  Solution solution = new P39_CombinationSum().new Solution();
-
-}
-  //leetcode submit region begin(Prohibit modification and deletion)
-class Solution {
-    public List<List<Integer>> combinationSum(int[] candidates, int target) {
-
+ * @author fabian
+ * @date 2022-08-17 11:33:00
+ */
+public class P39_CombinationSum {
+    public static void main(String[] args) {
+        Solution solution = new P39_CombinationSum().new Solution();
+        System.out.println(solution.combinationSum(new int[]{2, 3, 6, 7}, 7));
     }
-}
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public List<List<Integer>> combinationSum(int[] candidates, int target) {
+            List<List<Integer>> ans = new ArrayList<>();
+            List<Integer> combine = new ArrayList<>();
+            dfs(candidates, target, ans, combine, 0);
+            return ans;
+        }
+
+        public void dfs(int[] candidates, int target, List<List<Integer>> ans, List<Integer> combine, int idx) {
+            if (idx == candidates.length) {
+                return;
+            }
+            if (target == 0) {
+                ans.add(new ArrayList<>(combine));
+                return;
+            }
+            // 直接跳过
+            dfs(candidates, target, ans, combine, idx + 1);
+            // 选择当前数
+            if (target - candidates[idx] >= 0) {
+                combine.add(candidates[idx]);
+                dfs(candidates, target - candidates[idx], ans, combine, idx);
+                combine.remove(combine.size() - 1);
+            }
+        }
+    }
 //leetcode submit region end(Prohibit modification and deletion)
 
-  }
+}

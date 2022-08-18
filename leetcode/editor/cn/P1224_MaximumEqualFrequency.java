3773d0403bf7eff1c493e076e85a96340eb8e89d
fabian4 <fabian4@163.com>
2022-08-18 15:03:29 +0800

    update

--- leetcode/editor/cn/P1224_MaximumEqualFrequency.java
+++ leetcode/editor/cn/P1224_MaximumEqualFrequency.java
@@ -40,8 +40,8 @@ package leetcode.editor.cn;
 
 //最大相等频率
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author fabian
@@ -56,7 +56,24 @@ public class P1224_MaximumEqualFrequency {
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public int maxEqualFreq(int[] nums) {
-
+            Map<Integer, Integer> freq = new HashMap<>();
+            Map<Integer, Integer> count = new HashMap<>();
+            int res = 0, maxFreq = 0;
+            for (int i = 0; i < nums.length; i++) {
+                if (count.getOrDefault(nums[i], 0) > 0) {
+                    freq.put(count.get(nums[i]), freq.get(count.get(nums[i])) - 1);
+                }
+                count.put(nums[i], count.getOrDefault(nums[i], 0) + 1);
+                maxFreq = Math.max(maxFreq, count.get(nums[i]));
+                freq.put(count.get(nums[i]), freq.getOrDefault(count.get(nums[i]), 0) + 1);
+                boolean ok = maxFreq == 1 ||
+                        freq.get(maxFreq) * maxFreq + freq.get(maxFreq - 1) * (maxFreq - 1) == i + 1 && freq.get(maxFreq) == 1 ||
+                        freq.get(maxFreq) * maxFreq + 1 == i + 1 && freq.get(1) == 1;
+                if (ok) {
+                    res = Math.max(res, i + 1);
+                }
+            }
+            return res;
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)

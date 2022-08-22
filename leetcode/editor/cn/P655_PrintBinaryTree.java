06e75a52add1188538280b9f8d6a2c2db8467844
fabian4 <fabian4@163.com>
2022-08-22 14:49:44 +0800

    update

--- leetcode/editor/cn/P655_PrintBinaryTree.java
+++ leetcode/editor/cn/P655_PrintBinaryTree.java
@@ -88,33 +88,40 @@ public class P655_PrintBinaryTree {
      */
     class Solution {
         public List<List<String>> printTree(TreeNode root) {
-            if (root == null) {
-                return null;
-            }
-            int height = getHeight(root);
-            List<List<String>> ans = new ArrayList<>(height + 1);
-            Deque<TreeNode> stack = new ArrayDeque<>();
-            stack.push(root);
-            while (!stack.isEmpty()) {
-                int size = stack.size();
-                for (int i = 0; i < size; i++) {
-                    TreeNode temp = stack.pop();
-                    if (temp.left != null) {
-                        stack.push(temp.left);
-                    }
-                    if (temp.right != null) {
-                        stack.push(temp.right);
-                    }
+            int height = calDepth(root);
+            int m = height + 1;
+            int n = (1 << (height + 1)) - 1;
+            List<List<String>> res = new ArrayList<List<String>>();
+            for (int i = 0; i < m; i++) {
+                List<String> row = new ArrayList<String>();
+                for (int j = 0; j < n; j++) {
+                    row.add("");
                 }
+                res.add(row);
             }
-            return ans;
+            dfs(res, root, 0, (n - 1) / 2, height);
+            return res;
         }
 
-        public int getHeight(TreeNode root) {
-            if (root == null) {
-                return 0;
+        public int calDepth(TreeNode root) {
+            int h = 0;
+            if (root.left != null) {
+                h = Math.max(h, calDepth(root.left) + 1);
             }
-            return 1 + Math.max(getHeight(root.left), getHeight(root.right));
+            if (root.right != null) {
+                h = Math.max(h, calDepth(root.right) + 1);
+            }
+            return h;
+        }
+
+        public void dfs(List<List<String>> res, TreeNode root, int r, int c, int height) {
+            res.get(r).set(c, Integer.toString(root.val));
+            if (root.left != null) {
+                dfs(res, root.left, r + 1, c - (1 << (height - r - 1)), height);
+            }
+            if (root.right != null) {
+                dfs(res, root.right, r + 1, c + (1 << (height - r - 1)), height);
+            }
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)

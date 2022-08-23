62903138d868a40df3afb64c7c0cd33a02b95207
fabian4 <fabian4@163.com>
2022-08-23 10:43:20 +0800

    update

--- leetcode/editor/cn/P12_IntegerToRoman.java
+++ leetcode/editor/cn/P12_IntegerToRoman.java
@@ -89,7 +89,27 @@ public class P12_IntegerToRoman {
 
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
+
+        int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] symbols = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
         public String intToRoman(int num) {
+            StringBuffer roman = new StringBuffer();
+            for (int i = 0; i < values.length; ++i) {
+                int value = values[i];
+                String symbol = symbols[i];
+                while (num >= value) {
+                    num -= value;
+                    roman.append(symbol);
+                }
+                if (num == 0) {
+                    break;
+                }
+            }
+            return roman.toString();
+        }
+
+        public String _intToRoman(int num) {
             Map<Integer, String> map = new HashMap<>(7);
             map.put(1, "I");
             map.put(5, "V");

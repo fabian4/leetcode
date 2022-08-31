896cfa2c24135cbe4ddb5236fb15b77ed0bdadc6
fabian4 <fabian4@163.com>
2022-08-31 17:39:53 +0800

    update

--- leetcode/editor/cn/P47_PermutationsIi.java
+++ leetcode/editor/cn/P47_PermutationsIi.java
@@ -36,6 +36,8 @@ package leetcode.editor.cn;
 //全排列 II
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -52,26 +54,31 @@ public class P47_PermutationsIi {
     class Solution {
         public List<List<Integer>> permuteUnique(int[] nums) {
             List<List<Integer>> ans = new ArrayList<>();
+            if (nums.length == 0) {
+                return ans;
+            }
             boolean[] flag = new boolean[nums.length];
+            Arrays.sort(nums);
             backTrace(nums, flag, ans, new ArrayList<>());
             return ans;
         }
 
         void backTrace(int[] nums, boolean[] flag, List<List<Integer>> ans, List<Integer> output) {
             if (output.size() == nums.length) {
-                if(!ans.contains(output)){
+                if (!ans.contains(output)) {
                     ans.add(new ArrayList<>(output));
                 }
                 return;
             }
             for (int i = 0; i < nums.length; i++) {
-                if (!flag[i]) {
-                    output.add(nums[i]);
-                    flag[i] = true;
-                    backTrace(nums, flag, ans, output);
-                    flag[i] = false;
-                    output.remove(output.size() - 1);
+                if (flag[i] || (i > 0 && nums[i] == nums[i - 1] && !flag[i - 1])) {
+                    continue;
                 }
+                output.add(nums[i]);
+                flag[i] = true;
+                backTrace(nums, flag, ans, output);
+                flag[i] = false;
+                output.remove(output.size() - 1);
             }
 
         }

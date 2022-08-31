da9b884791c23db6dab7c02f92d643cb1ccdeec7
fabian4 <fabian4@163.com>
2022-08-31 17:07:03 +0800

    update

---
+++ leetcode/editor/cn/P47_PermutationsIi.java
@@ -0,0 +1,58 @@
+//给定一个可包含重复数字的序列 nums ，按任意顺序 返回所有不重复的全排列。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：nums = [1,1,2]
+//输出：
+//[[1,1,2],
+// [1,2,1],
+// [2,1,1]]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：nums = [1,2,3]
+//输出：[[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= nums.length <= 8 
+// -10 <= nums[i] <= 10 
+// 
+//
+// Related Topics 数组 回溯 👍 1176 👎 0
+
+
+package leetcode.editor.cn;
+
+//全排列 II
+
+import java.util.List;
+
+/**
+ * @author fabian
+ * @date 2022-08-31 17:06:20
+ */
+public class P47_PermutationsIi {
+    public static void main(String[] args) {
+        Solution solution = new P47_PermutationsIi().new Solution();
+
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public List<List<Integer>> permuteUnique(int[] nums) {
+
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}

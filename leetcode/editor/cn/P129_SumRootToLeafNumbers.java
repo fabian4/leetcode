ae6cb9e8edefefab3c9187fd93fc7478b44cf05b
fabian4 <fabian4@163.com>
2022-09-02 11:22:18 +0800

    update

--- leetcode/editor/cn/P129_SumRootToLeafNumbers.java
+++ leetcode/editor/cn/P129_SumRootToLeafNumbers.java
@@ -85,7 +85,19 @@ public class P129_SumRootToLeafNumbers {
      */
     class Solution {
         public int sumNumbers(TreeNode root) {
+            return dfs(root, 0);
+        }
 
+        public int dfs(TreeNode root, int prevSum) {
+            if (root == null) {
+                return 0;
+            }
+            int sum = prevSum * 10 + root.val;
+            if (root.left == null && root.right == null) {
+                return sum;
+            } else {
+                return dfs(root.left, sum) + dfs(root.right, sum);
+            }
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)

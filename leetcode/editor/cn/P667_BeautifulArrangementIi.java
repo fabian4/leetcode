30f587d2e042d6565dddf7d0efc23295ad92f9d0
fabian4 <fabian4@163.com>
2022-09-08 10:55:22 +0800

    update

--- leetcode/editor/cn/P667_BeautifulArrangementIi.java
+++ leetcode/editor/cn/P667_BeautifulArrangementIi.java
@@ -55,7 +55,21 @@ public class P667_BeautifulArrangementIi {
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public int[] constructArray(int n, int k) {
-
+            int[] answer = new int[n];
+            int idx = 0;
+            for (int i = 1; i < n - k; ++i) {
+                answer[idx] = i;
+                ++idx;
+            }
+            for (int i = n - k, j = n; i <= j; ++i, --j) {
+                answer[idx] = i;
+                ++idx;
+                if (i != j) {
+                    answer[idx] = j;
+                    ++idx;
+                }
+            }
+            return answer;
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)

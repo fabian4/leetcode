f3e85d146218e085b12da2c9c2f7a703d40d0232
fabian4 <fabian4@163.com>
2022-09-08 10:54:19 +0800

    update

--- leetcode/editor/cn/P526_BeautifulArrangement.java
+++ leetcode/editor/cn/P526_BeautifulArrangement.java
@@ -45,6 +45,9 @@ package leetcode.editor.cn;
 
 //优美的排列
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author fabian
  * @date 2022-09-08 10:51:35
@@ -57,8 +60,39 @@ public class P526_BeautifulArrangement {
 
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
-        public int countArrangement(int n) {
+        List<Integer>[] match;
+        boolean[] vis;
+        int num;
 
+        public int countArrangement(int n) {
+            vis = new boolean[n + 1];
+            match = new List[n + 1];
+            for (int i = 0; i <= n; i++) {
+                match[i] = new ArrayList<>();
+            }
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= n; j++) {
+                    if (i % j == 0 || j % i == 0) {
+                        match[i].add(j);
+                    }
+                }
+            }
+            backtrack(1, n);
+            return num;
+        }
+
+        public void backtrack(int index, int n) {
+            if (index == n + 1) {
+                num++;
+                return;
+            }
+            for (int x : match[index]) {
+                if (!vis[x]) {
+                    vis[x] = true;
+                    backtrack(index + 1, n);
+                    vis[x] = false;
+                }
+            }
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)

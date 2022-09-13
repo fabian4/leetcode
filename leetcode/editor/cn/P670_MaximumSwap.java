68bfc6da7cc56bc8a00c2e82a8acfc501f131524
fabian4 <fabian4@163.com>
2022-09-13 14:49:01 +0800

    update

---
+++ leetcode/editor/cn/P670_MaximumSwap.java
@@ -0,0 +1,50 @@
+//给定一个非负整数，你至多可以交换一次数字中的任意两位。返回你能得到的最大值。 
+//
+// 示例 1 : 
+//
+// 
+//输入: 2736
+//输出: 7236
+//解释: 交换数字2和数字7。
+// 
+//
+// 示例 2 : 
+//
+// 
+//输入: 9973
+//输出: 9973
+//解释: 不需要交换。
+// 
+//
+// 注意: 
+//
+// 
+// 给定数字的范围是 [0, 10⁸] 
+// 
+//
+// Related Topics 贪心 数学 👍 319 👎 0
+
+
+package leetcode.editor.cn;
+
+//最大交换
+
+/**
+ * @author fabian
+ * @date 2022-09-13 14:48:32
+ */
+public class P670_MaximumSwap {
+    public static void main(String[] args) {
+        Solution solution = new P670_MaximumSwap().new Solution();
+
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int maximumSwap(int num) {
+
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}

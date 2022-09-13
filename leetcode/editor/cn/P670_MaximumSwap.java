4663692406716b73957ad80d7a217132b32c1448
fabian4 <fabian4@163.com>
2022-09-13 14:53:24 +0800

    update

--- leetcode/editor/cn/P670_MaximumSwap.java
+++ leetcode/editor/cn/P670_MaximumSwap.java
@@ -42,7 +42,30 @@ public class P670_MaximumSwap {
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public int maximumSwap(int num) {
+            char[] charArray = String.valueOf(num).toCharArray();
+            int n = charArray.length;
+            int maxIdx = n - 1;
+            int idx1 = -1, idx2 = -1;
+            for (int i = n - 1; i >= 0; i--) {
+                if (charArray[i] > charArray[maxIdx]) {
+                    maxIdx = i;
+                } else if (charArray[i] < charArray[maxIdx]) {
+                    idx1 = i;
+                    idx2 = maxIdx;
+                }
+            }
+            if (idx1 >= 0) {
+                swap(charArray, idx1, idx2);
+                return Integer.parseInt(new String(charArray));
+            } else {
+                return num;
+            }
+        }
 
+        public void swap(char[] charArray, int i, int j) {
+            char temp = charArray[i];
+            charArray[i] = charArray[j];
+            charArray[j] = temp;
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)

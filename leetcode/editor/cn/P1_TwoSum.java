3541a02c37f63a99d74b1dc3d9931ae75f04e148
fabian4 <fabian4@163.com>
2023-02-01 20:45:55 +0800

    update

--- leetcode/editor/cn/P1_TwoSum.java
+++ leetcode/editor/cn/P1_TwoSum.java
@@ -46,25 +46,33 @@
 // Related Topics 数组 哈希表 👍 16226 👎 0
 
 
-  package leetcode.editor.cn;
+package leetcode.editor.cn;
 
-  //两数之和
+//两数之和
 
-  /**
-  * @author fabian
-  * @date 2023-02-01 20:32:08
-  */
-  public class P1_TwoSum{
-  public static void main(String[] args) {
-  Solution solution = new P1_TwoSum().new Solution();
-
-}
-  //leetcode submit region begin(Prohibit modification and deletion)
-class Solution {
-    public int[] twoSum(int[] nums, int target) {
+/**
+ * @author fabian
+ * @date 2023-02-01 20:32:08
+ */
+public class P1_TwoSum {
+    public static void main(String[] args) {
+        Solution solution = new P1_TwoSum().new Solution();
 
     }
-}
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int[] twoSum(int[] nums, int target) {
+            for (int i = 0; i < nums.length; i++) {
+                for (int j = i + 1; j < nums.length; j++) {
+                    if (nums[i] + nums[j] == target) {
+                        return new int[]{i, j};
+                    }
+                }
+            }
+            return null;
+        }
+    }
 //leetcode submit region end(Prohibit modification and deletion)
 
-  }
+}

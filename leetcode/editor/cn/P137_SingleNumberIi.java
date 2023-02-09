9aa0cbc73bb743a701724c83da0f285bf6be2907
fabian4 <fabian4@163.com>
2023-02-09 16:24:17 +0800

    update

---
+++ leetcode/editor/cn/P137_SingleNumberIi.java
@@ -0,0 +1,60 @@
+//给你一个整数数组 nums ，除某个元素仅出现 一次 外，其余每个元素都恰出现 三次 。请你找出并返回那个只出现了一次的元素。 
+//
+// 你必须设计并实现线性时间复杂度的算法且不使用额外空间来解决此问题。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：nums = [2,2,3,2]
+//输出：3
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：nums = [0,1,0,1,0,1,99]
+//输出：99
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= nums.length <= 3 * 10⁴ 
+// -2³¹ <= nums[i] <= 2³¹ - 1 
+// nums 中，除某个元素仅出现 一次 外，其余每个元素都恰出现 三次 
+// 
+//
+// Related Topics 位运算 数组 👍 973 👎 0
+
+
+  package leetcode.editor.cn;
+
+  //只出现一次的数字 II
+
+  /**
+  * @author fabian
+  * @date 2023-02-09 16:20:56
+  */
+  public class P137_SingleNumberIi{
+  public static void main(String[] args) {
+  Solution solution = new P137_SingleNumberIi().new Solution();
+
+}
+  //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int singleNumber(int[] nums) {
+        int a = 0, b = 0;
+        for (int num : nums) {
+            b = ~a & (b ^ num);
+            a = ~b & (a ^ num);
+        }
+        return b;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+  }
